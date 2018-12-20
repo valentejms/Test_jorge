afcd10298a52fff7167bbf6479573b88078b178f
Jorge Valente <valent.jms@gamil.com>
2018-12-20 16:33:56 +0000

    mudanças ao nivel da estrutura

--- UniversidadeAveiro/index.php
+++ UniversidadeAveiro/index.php
@@ -8,20 +8,9 @@
 </head>
 
 <body>
-	<div class="header"> 
-    	<img src="imagens/header_UA.png">
-    </div>
-    
-    <div class="menu"> 
-    	<ul>
-            <li style="padding-left:200px"><a href="">apresentação</a></li>
-            <li><a href="">organização</a></li>
-            <li><a href="">departamentos</a></li>
-  			<li><a href="">ensino</a></li>
-            <li><a href="">id</a></li>
-            <li><a href="">cooperação</a></li>
-		</ul>     
-    </div>     
+    <?php
+        include_once('menu.php');
+    ?>
     <div class="limpar"></div> <br>
     
     <div class="c31">
--- UniversidadeAveiro/menu.php
+++ UniversidadeAveiro/menu.php
@@ -1,10 +1,14 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+	<div class="header"> 
+    	<img src="imagens/header_UA.png">
+    </div>
+    
+    <div class="menu"> 
+    	<ul>
+            <li style="padding-left:200px"><a href="">apresentação</a></li>
+            <li><a href="">organização</a></li>
+            <li><a href="">departamentos</a></li>
+  			<li><a href="">ensino</a></li>
+            <li><a href="">id</a></li>
+            <li><a href="">cooperação</a></li>
+		</ul>     
+    </div> 
