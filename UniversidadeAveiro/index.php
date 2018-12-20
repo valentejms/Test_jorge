e89bacdce1e3cfe0ad2ae6879e5666b3acaa50e3
Jorge Valente <valent.jms@gamil.com>
2018-12-20 15:52:55 +0000

    Copia em html do site da UA
    Exercicio pratico de replicar o site da Universidade de Aveiro em HMTL/CSS

---
+++ UniversidadeAveiro/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Universidade de Aveiro</title>
+<link rel="stylesheet" type="text/css" href="estilos.css">
+<meta charset="utf-8">
+</head>
+
+<body>
+	<div class="header"> 
+    	<img src="imagens/header_UA.png">
+    </div>
+    
+    <div class="menu"> 
+    	<ul>
+            <li style="padding-left:200px"><a href="">apresentação</a></li>
+            <li><a href="">organização</a></li>
+            <li><a href="">departamentos</a></li>
+  			<li><a href="">ensino</a></li>
+            <li><a href="">id</a></li>
+            <li><a href="">cooperação</a></li>
+		</ul>     
+    </div>     
+    <div class="limpar"></div> <br>
+    
+    <div class="c31">
+		<p>Coluna 1</p>
+    </div>
+    <div class="c32">
+		<p>Coluna 2</p>
+    </div>
+    <div class="c33">
+		<p>Coluna 3</p>
+    </div>
+    
+</body>
+</html>
